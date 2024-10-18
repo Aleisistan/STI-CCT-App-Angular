dfdfc9bac70845f9e0a7c78fc131a5ed1fc28ef5
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-10-18 17:59:38 -0300

    hasta routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AboutUsComponent } from "./about-us/about-us.component";
 import { OrderListComponent } from "./order-list/order-list.component";
 import { UserListComponent } from "./user-list/user-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent, OrderListComponent],
+  imports: [RouterOutlet, UserListComponent, OrderListComponent, AboutUsComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',redirectTo: '/about',pathMatch: 'full' },
+    { path: 'users', component: UserListComponent },
+    { path: 'about', component: AboutUsComponent },
+    { path: 'orders', component: OrderListComponent}
+
+];
+NgModule({
+    declarations: [
+        AppComponent, AboutUsComponent, UserListComponent, OrderListComponent
+        ],
+    imports: [RouterModule.forRoot(routes), AppComponent, UserListComponent, AboutUsComponent, OrderListComponent],
+    exports: [RouterModule],
+    bootstrap: [AppComponent]
+})
+export class AppRoutingModule {}
