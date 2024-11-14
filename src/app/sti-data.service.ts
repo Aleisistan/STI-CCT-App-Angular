df7b87739fd79b16658bc8204c175d68ab1d1d2c
Alejandro Toloza <carlosalejandrotoloza@gmail.com>
2024-11-14 11:58:03 -0300

    environ corregido

--- src/app/sti-data.service.ts
+++ src/app/sti-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../environment/environment';
+import { environment } from '../environments/environment';
 import { Order } from './order-list/order';
 import { User } from './user-list/user';
 
